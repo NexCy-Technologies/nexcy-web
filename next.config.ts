2c3e830059eae8f4deba0a84bfb540b5bd5b0670
Dinuka Lakshan <74536249+DrkCyph7@users.noreply.github.com>
2026-09-19 12:30:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.1.142'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.thum.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
